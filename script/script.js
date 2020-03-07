bfc42cebcf3ff5ae67120bf28c43da67cef997ee
Mostafa <mustafa.husseiny@gmail.com>
2020-03-07 02:51:48 +0100

    adding new features

--- script/script.js
+++ script/script.js
@@ -40,7 +40,39 @@ userReg.onclick = () => {
 const loginFromReg = document.getElementsByClassName('login-link')[0];
 
 loginFromReg.onclick = () => {
-   // alert('hi');
     mainReg.classList.remove('open-reg');
     loginFormat.classList.add('open-login');
 }
+
+function loadMainbProducts() {
+    const productsContainer = document.getElementsByClassName('products-container')[0];
+    const productTemplate = document.getElementById('product-template');
+    const productsUrl = 'https://raw.githubusercontent.com/freelancer2020/hexa/master/products.json';
+    const arr = [];
+    //alert('hi');
+    fetch(productsUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(pro => {
+        for  (let i = 0; i < pro.length; i++) {
+           let frame = productTemplate.content.cloneNode(true);
+            productsContainer.append(frame);
+            arr.push(pro[i]);
+        }
+    })
+    .then(() => {
+        const lam = document.getElementsByClassName('product-box');
+        let i, x;
+        for (i = 0; i < arr.length; i++) {
+            const image = document.createElement("img");
+            image.src = arr[i].productPhoto;
+            image.classList.add('resizeImage');
+            lam[i].firstElementChild.append(image);
+            lam[i].firstElementChild.nextElementSibling.append(arr[i].productName);
+            lam[i].firstElementChild.nextElementSibling.nextElementSibling.append(arr[i].productPrice);
+        }
+    })
+    
+}
+window.onload = loadMainbProducts;
